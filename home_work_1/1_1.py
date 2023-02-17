775ec609596b3007c590cc918a412c75e660758f
Ekaterina <sevic_bag@mail.ru>
2023-02-17 19:03:51 +0300

    дз1

--- home_work_1/1_1.py
+++ home_work_1/1_1.py
@@ -2,3 +2,12 @@
 # *Пример:*
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0) |
+
+a = input("Введите число: ")
+a = list(a)
+a = list(map(int, a))
+sum = 0
+for i in range(len(a)):
+    sum+=a[i]
+
+print(sum)
